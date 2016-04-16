4b248f19de5fdfcd717dc6ce33787301c5b597f4
Miles Zimmerman <miles@maztek.io>
2016-04-16 00:11:03 -0700

    Adding events, affiliations show views

---
+++ app/controllers/events/show.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  firstArtifact: Ember.computed('model.artifacts.@each.firstObject', function() {
+    return this.get('model.artifacts').get('firstObject')
+  })
+});
