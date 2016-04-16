4b2b22ea9b1534467edf6be6dcf8d3e31884e9a1
Miles Zimmerman <miles@maztek.io>
2016-04-15 23:20:54 -0700

    Updating pagination component to only show pageLimit

--- app/components/page-numbers.js
+++ app/components/page-numbers.js
@@ -8,8 +8,10 @@ export default Ember.Component.extend({
   recordCount: 0,
   pageSize: 10,
   currentPage: 1,
-  maxVisiblePages: 10,
+  visiblePageBuffer: 5,
 
+
+  // Helpers
   totalPages: Ember.computed('recordCount', 'pageSize', function() {
     let pageSize = this.get('pageSize') || 1; // Don't divide by zero
     return Math.round(this.get('recordCount') / pageSize) || 1;
@@ -20,9 +22,24 @@ export default Ember.Component.extend({
     return this.get('currentPage') < this.get('totalPages');
   }),
 
-  pageArray: Ember.computed('totalPages', function() {
-    let range = new Array(this.get('totalPages'));
-    for (let i = 0; i < range.length; i += 1) {
+  pageArray: Ember.computed('totalPages', 'currentPage', 'visiblePageBuffer', function() {
+    let pages = this.get('totalPages');
+    let buffer = this.get('visiblePageBuffer');
+    let current = this.get('currentPage');
+    let rangeStart = 1;
+    let rangeEnd = pages;
+
+    if ((current - buffer) > 1)     { rangeStart = (current - buffer); }
+    if ((current + buffer) < pages) { rangeEnd   = (current + buffer); }
+    if ((current < buffer) && ((buffer * 2) < pages)) {
+      rangeEnd = buffer * 2;
+    }
+    if (((rangeEnd - current) < buffer) && (buffer * 2) < pages) {
+      rangeStart = pages - (buffer * 2);
+    }
+
+    let range = new Array(rangeEnd - rangeStart);
+    for (let i = (rangeStart - 1); i < rangeEnd; i += 1) {
       range[i] = i+1;
     }
     return range;
