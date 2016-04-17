222123186f2069223c689a306653099ecc3272f8
Miles Zimmerman <miles@maztek.io>
2016-04-17 01:56:43 -0700

    Show error messages for login failure

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  errorMessage: null,
 
   actions: {
     authenticate() {
       let credentials = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:jwt', credentials).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
+      this.get('session').authenticate('authenticator:jwt', credentials).catch((res) => {
+        if (res.errors[0].details === "Invalid credentials") {
+          this.set('errorMessage', "The email address or password is unrecognized.");
+        } else {
+          this.set('errorMessage', "There was a problem signing you in. Please try again later.");
+        }
       });
     }
   }
