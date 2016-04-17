bf10fd128f109f489bc8deb96a23421a3e8cd3f7
Miles Zimmerman <miles@maztek.io>
2016-04-17 02:07:11 -0700

    Show progress indicator for login, and fix some styling on app loading indicator

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   errorMessage: null,
+  submitted: false,
 
   actions: {
     authenticate() {
+      this.set('submitted', true);
       let credentials = this.getProperties('identification', 'password');
       this.get('session').authenticate('authenticator:jwt', credentials).catch((res) => {
         if (res.errors[0].details === "Invalid credentials") {
@@ -13,6 +15,7 @@ export default Ember.Controller.extend({
         } else {
           this.set('errorMessage', "There was a problem signing you in. Please try again later.");
         }
+        this.set('submitted', false);
       });
     }
   }
